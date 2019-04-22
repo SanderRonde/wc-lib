f623fbf81acc04bc060552d45d7137ea1f061d12
Sander Ronde <awsdfgvhbjn@gmail.com>
2019-04-23 01:29:55 +0200

    add entrypoints

---
+++ src/wc.ts
@@ -0,0 +1,6 @@
+import { ConfigurableWebComponent } from './lib/configurable';
+export { config, ConfigurableWebComponent } from './lib/configurable';
+export { CHANGE_TYPE, TemplateFn } from './lib/base';
+export { Props, PROP_TYPE } from './lib/props';
+
+export default ConfigurableWebComponent;

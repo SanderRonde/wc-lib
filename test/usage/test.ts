95698b7b1f38860e83b2f5280dc953b52d91164c
Sander Ronde <awsdfgvhbjn@gmail.com>
2019-05-21 01:17:51 +0200

    add some console.logs

--- test/usage/test.ts
+++ test/usage/test.ts
@@ -5,10 +5,12 @@ import * as path from 'path';
 
 export const USAGE_TEST_PORT = 1251;
 
+console.log('Starting http-server');
 const httpServer = createServer({
 	root: path.join(__dirname, '../../')
 });
 httpServer.listen(USAGE_TEST_PORT, async () => {
+	console.log('Started http-server, running cypress');
 	cypress.run({
 		...JSON.parse(await fs.readFile(path.join(__dirname, '../../',
 		'cypress.json'), {
@@ -16,6 +18,7 @@ httpServer.listen(USAGE_TEST_PORT, async () => {
 		})),
 		record: process.argv.indexOf('--no-record') ? false : true
 	}).then((results) => {
+		console.log('Done');
 		httpServer.close();
 		process.exit(results.totalFailed);
 	});

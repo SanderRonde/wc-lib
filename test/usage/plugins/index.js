e573c328cd71206992046128923f15124b221aa5
Sander Ronde <awsdfgvhbjn@gmail.com>
2019-05-20 23:12:34 +0200

    add plugin file

---
+++ test/usage/plugins/index.js
@@ -0,0 +1,9 @@
+module.exports = (on, config) => {
+	on('before:browser:launch', (browser = {}, args) => {
+		if (browser.name === 'chrome') {
+			args.push('--disable-site-isolation-trials');
+	  
+			return args
+		  }
+	})
+}

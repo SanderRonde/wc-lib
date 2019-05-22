5366df20f5b26e04fee7cd26e892a8209f856b9c
Sander Ronde <awsdfgvhbjn@gmail.com>
2019-05-22 22:35:40 +0200

    add definer.ts tests

---
+++ test/usage/integration/classes/definer/definer.fixture.ts
@@ -0,0 +1,4 @@
+import { DefineMetadata } from "../../../../../src/wclib.js";
+import "../elements/parent-element.js";
+
+(window as any).DefineMetadata = DefineMetadata;
---
+++ test/usage/integration/classes/definer/definer.spec.ts
@@ -0,0 +1,104 @@
+/// <reference types="Cypress" />
+
+import { expectMethodExists, expectPropertyExists } from "../../../lib/assertions";
+import { TestParentWindow } from "../elements/parent-element";
+import { DefineMetadata } from "../../../../../src/wclib";
+import { TestWindow } from "../elements/test-element";
+
+interface DefineMetaDataWindow extends Window {
+	DefineMetadata: typeof DefineMetadata;
+}
+
+context('Definer', function() {
+	before(() => {
+		cy.visit('http://localhost:1251/test/usage/integration/classes/definer/definer.fixture.html');
+	});
+
+	context('Properties/Methods', () => {
+		it('exposes a static .dependencies property', () => {
+			cy.window().then((window: TestWindow) => {
+				expectPropertyExists(window.TestElement, 'dependencies');
+			});
+		});
+		it('exposes a static .is property', () => {
+			cy.window().then((window: TestWindow) => {
+				expectPropertyExists(window.TestElement, 'is');
+			});
+		});
+		it('exposes a static #define method', () => {
+			cy.window().then((window: TestWindow) => {
+				expectMethodExists(window.TestElement, 'define');
+			});
+		});
+	});
+	context('Defining', () => {
+		beforeEach(() => {
+			cy.visit('http://localhost:1251/test/usage/integration/classes/definer/definer.fixture.html');
+		});
+		it('defines the component when calling #define', () => {
+			cy.window().then((window: TestParentWindow) => {
+				expect(window.customElements.get('parent-element'))
+					.to.be.undefined;
+				window.ParentElement.define();
+				expect(window.customElements.get('parent-element'))
+					.to.be.equal(window.ParentElement);
+			});
+		});
+		it('defines any dependencies when calling #define', () => {
+			cy.window().then((window: TestParentWindow & TestWindow) => {
+				expect(window.customElements.get('parent-element'))
+					.to.be.undefined;
+				expect(window.customElements.get('test-element'))
+					.to.be.undefined;
+				window.ParentElement.define();
+				expect(window.customElements.get('parent-element'))
+					.to.be.equal(window.ParentElement);
+				expect(window.customElements.get('test-element'))
+					.to.be.equal(window.TestElement);
+			});
+		});
+	});
+	context('Metadata', () => {
+		beforeEach(() => {
+			cy.visit('http://localhost:1251/test/usage/integration/classes/definer/definer.fixture.html');
+		});
+		it('has defined 0 elements initially', () => {
+			cy.window().then((window: DefineMetaDataWindow) => {
+				expect(window.DefineMetadata).to.have.property('defined')
+					.to.be.equal(0, 'no elements are defined initially');
+			});
+		});
+		it('increases the counter when a component is defined', () => {
+			cy.window().then((window: DefineMetaDataWindow & TestParentWindow) => {
+				expect(window.DefineMetadata).to.have.property('defined')
+					.to.be.equal(0, 'no elements are defined initially');
+				window.ParentElement.define();
+				expect(window.DefineMetadata).to.have.property('defined')
+					.to.be.equal(2, 'new elements were defined');
+			});
+		});
+		it('calls listeners when a component is defined', () => {
+			cy.window().then((window: DefineMetaDataWindow & TestParentWindow) => {
+				const listener = cy.spy((amount: number) => {
+					expect(amount).to.be.at.least(1,
+						'at least 1 element has been defined');
+				}) as any;
+				window.DefineMetadata.onDefine(listener);
+
+				expect(listener).to.not.be.called;
+				window.ParentElement.define();
+				expect(listener).to.be.calledTwice;
+			});
+		});
+		it('calls onReach when the desired amount has been reached', () => {
+			cy.window().then((window: DefineMetaDataWindow & TestParentWindow) => {
+				const listener = cy.spy() as any;
+				window.DefineMetadata.onReach(2, listener);
+
+				expect(listener).to.not.be.called;
+				window.ParentElement.define();
+				expect(listener).to.be.calledOnce;
+			});
+		});
+	});
+});

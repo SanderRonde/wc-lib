6fa35b2d815ca8bf5f8a8a102308c490233e44ac
Sander Ronde <awsdfgvhbjn@gmail.com>
2019-07-29 00:47:17 +0200

    temporarily remove wcib.all.ts

--- src/wclib.all.ts
+++ src/wclib.all.ts
@@ -2,28 +2,28 @@
  * All exports this library has to offer
  */
 
-export * from './classes/full.js';
-export * from './classes/partial';
-export * from './classes/parts.js';
-export * from './classes/types.js';
+// export * from './classes/full.js';
+// export * from './classes/partial';
+// export * from './classes/parts.js';
+// export * from './classes/types.js';
 
-export * from './lib/base.js'
-export * from './lib/component.js'
-export * from './lib/configurable.js'
-export * from './lib/css.js'
-export * from './lib/custom-css-manager.js'
-export * from './lib/definer.js'
-export * from './lib/hierarchy-manager.js'
-export * from './lib/i18n-manager.js'
-export * from './lib/jsx-render.js'
-export * from './lib/listener.js'
-export * from './lib/listeners.js'
-export * from './lib/props.js'
-export * from './lib/shared.js'
-export * from './lib/template-fn.js'
-export * from './lib/template-manager.js'
-export * from './lib/theme-manager.js'
-export * from './lib/util.js'
-export * from './lib/webcomponent-types.js'
+// export * from './lib/base.js'
+// export * from './lib/component.js'
+// export * from './lib/configurable.js'
+// export * from './lib/css.js'
+// export * from './lib/custom-css-manager.js'
+// export * from './lib/definer.js'
+// export * from './lib/hierarchy-manager.js'
+// export * from './lib/i18n-manager.js'
+// export * from './lib/jsx-render.js'
+// export * from './lib/listener.js'
+// export * from './lib/listeners.js'
+// export * from './lib/props.js'
+// export * from './lib/shared.js'
+// export * from './lib/template-fn.js'
+// export * from './lib/template-manager.js'
+// export * from './lib/theme-manager.js'
+// export * from './lib/util.js'
+// export * from './lib/webcomponent-types.js'
  
-export * from './tasks/typed-css.js';
+// export * from './tasks/typed-css.js';

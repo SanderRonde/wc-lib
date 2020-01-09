8878f8284ca1ebee07a5a6a6a11a288d768c8540
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-01-10 00:43:37 +0100

    finish bundled examples

--- examples/gulpfile.ts
+++ examples/gulpfile.ts
@@ -1,4 +1,5 @@
 import { inlineTypedCSSPipe } from '../build/cjs/tasks/tasks';
+const gulpClean = require('gulp-clean');
 import * as replace from 'gulp-replace';
 import * as webpack from 'webpack';
 import * as rimraf from 'rimraf';
@@ -6,62 +7,71 @@ import * as fs from 'fs-extra';
 import * as gulp from 'gulp';
 import * as path from 'path';
 
-// Prepares the examples for hosting on gh-pages
-gulp.task('bundle', gulp.series(function clean() {
+// Bundle pages
+gulp.task('bundle', gulp.series(function remove() {
 	return new Promise((resolve) => {
-		rimraf(path.join(__dirname, 'temp'), (_err) => {
-			console.log(_err);
+		rimraf(path.join(__dirname, '../examples-bundled'), (_err) => {
 			// Ignore errors because if it doesn't exist
 			// it's been removed anyway
 			resolve();
 		});
 	});
 }, function copy() {
+	// Copy all source files to the bundled directory
 	return gulp.src([
 		'**/*.js',
-		'**/*.html'
+		'**/*.html',
+		'**/*.json',
+		'**/*.png',
 	], {
 		cwd: './',
 		base: './'
 	})
-	.pipe(gulp.dest('./temp/'));
+	.pipe(gulp.dest('../examples-bundled/'));
 }, function inlineCSS() {
+	// Inline all typed CSS
 	return gulp.src([
 		'**/*.css.js'
 	], {
-		cwd: './temp/',
-		base: './temp/'
+		cwd: '../examples-bundled/',
+		base: '../examples-bundled/'
 	})
 	.pipe(inlineTypedCSSPipe())
-	.pipe(gulp.dest('./temp'));
+	.pipe(gulp.dest('../examples-bundled'));
 }, function changeImports() {
+	// Change imports to be relative to the root (since
+	// it's been moved to bundled/)
 	return gulp.src([
 		'**/*.js',
 	], {
-		cwd: './temp/',
-		base: './temp/'
+		cwd: '../examples-bundled/',
+		base: '../examples-bundled/'
 	})
 	.pipe(replace(/\.\.\/\.\.\//g, 
 		'../../../'))
-	.pipe(gulp.dest('temp'));
+	.pipe(gulp.dest('bundled'));
 }, async function bundle() {
-	const dirs = (await Promise.all((await fs.readdir(path.join(__dirname, 'temp'))).map(async (file) => {
+	// GEt all directories in the bundled/ folder
+	const dirs = (await Promise.all((await fs.readdir(path.join(__dirname, '../examples-bundled'))).map(async (file) => {
 		return [file, await fs.stat(file)]
 	}))).filter(([ _, stat ]) => (stat as fs.Stats).isDirectory())
 		.map(([name, _]) => name as string);
-	console.log(dirs);
+
+	// For every dir, bundle the index.js file
 	return Promise.all(dirs.map((dir) => {
-		console.log(dir);
 		return new Promise((resolve, reject) => {
 			webpack({
 				mode: 'production',
-				entry: path.join(__dirname, 'temp', dir, 'index.js'),
+				entry: path.join(__dirname, '../examples-bundled', dir, 'index.js'),
 				output: {
-					path: path.join(__dirname, 'temp', dir),
-					filename: 'index2.js'
-				}
-			}, (err, stats) => {
-				console.log(err, stats.compilation.outputOptions.path);
+					path: path.join(__dirname, '../examples-bundled', dir),
+					filename: 'index.js'
+				},
+				optimization: {
+					usedExports: true
+				},
+				node: false
+			}, (err) => {
 				if (err) {
 					reject(err);
 					return;
@@ -70,4 +80,30 @@ gulp.task('bundle', gulp.series(function clean() {
 			});
 		});
 	}))
+}, function clean() {
+	// Clean all non-bundle files
+	return gulp.src([
+		'**/*.*',
+		'!**/index.js',
+		'!**/index.html',
+		'!**/*.json',
+		'!**/*.png',
+	], {
+		cwd: '../examples-bundled',
+		base: '../examples-bundled',
+		read: false
+	})
+	.pipe(gulpClean())
+}, function changeHTML() {
+	// Change the overview index page to display
+	// different text depending on whether it's bundled or not
+	return gulp.src([
+		'index.html'
+	], {
+		cwd: '../examples-bundled',
+		base: '../examples-bundled',
+	})
+	.pipe(replace(/hidden id="ifnotbundled"/g, 'id="ifnotbundled"'))
+	.pipe(replace(/id="ifbundled"/g, 'hidden id="ifbundled"'))
+	.pipe(gulp.dest('./'))
 }));

226e343a8e958eff43dfcc3a24427aa92b13eced
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-02-15 05:36:15 +0100

    generate server-sider-rendered examples (partial)

--- examples/gulpfile.ts
+++ examples/gulpfile.ts
@@ -1,4 +1,6 @@
 import { inlineTypedCSSPipe } from '../build/cjs/tasks/tasks';
+import { ssr } from '../build/cjs/lib/ssr/ssr';
+import { theme } from './tic-tac-toe/theme';
 const gulpClean = require('gulp-clean');
 import * as replace from 'gulp-replace';
 import * as webpack from 'webpack';
@@ -7,13 +9,13 @@ import * as fs from 'fs-extra';
 import * as gulp from 'gulp';
 import * as path from 'path';
 
-// Bundle pages
-gulp.task(
-    'bundle',
-    gulp.series(
+const EXCLUDED_DIRS = ['ssr', 'modules', 'bundled'];
+
+function bundleToDir(dir: string) {
+    return gulp.series(
         function remove() {
             return new Promise((resolve) => {
-                rimraf(path.join(__dirname, './bundled'), (_err) => {
+                rimraf(path.join(__dirname, dir), (_err) => {
                     // Ignore errors because if it doesn't exist
                     // it's been removed anyway
                     resolve();
@@ -23,38 +25,48 @@ gulp.task(
         function copy() {
             // Copy all source files to the bundled directory
             return gulp
-                .src(['**/*.js', '**/*.html', '**/*.json', '**/*.png'], {
-                    cwd: './',
-                    base: './',
-                })
-                .pipe(gulp.dest('./bundled'));
+                .src(
+                    [
+                        '**/*.js',
+                        '**/*.html',
+                        '**/*.json',
+                        '**/*.png',
+                        '!ssr/**',
+                        '!bundled/**',
+                    ],
+                    {
+                        cwd: './',
+                        base: './',
+                    }
+                )
+                .pipe(gulp.dest(dir));
         },
         function inlineCSS() {
             // Inline all typed CSS
             return gulp
                 .src(['**/*.css.js'], {
-                    cwd: './bundled',
-                    base: './bundled',
+                    cwd: dir,
+                    base: dir,
                 })
                 .pipe(inlineTypedCSSPipe())
-                .pipe(gulp.dest('./bundled'));
+                .pipe(gulp.dest(dir));
         },
         function changeImports() {
             // Change imports to be relative to the root (since
-            // it's been moved to bundled/)
+            // it's been moved to dir/)
             return gulp
                 .src(['**/*.js'], {
-                    cwd: './bundled',
-                    base: './bundled',
+                    cwd: dir,
+                    base: dir,
                 })
                 .pipe(replace(/\.\.\/modules/g, '../../modules'))
-                .pipe(gulp.dest('bundled'));
+                .pipe(gulp.dest(dir));
         },
         async function bundle() {
             // Get all directories in the bundled/ folder
             const dirs = (
                 await Promise.all(
-                    (await fs.readdir(path.join(__dirname, './bundled'))).map(
+                    (await fs.readdir(path.join(__dirname, dir))).map(
                         async (file) => {
                             return [file, await fs.stat(file)];
                         }
@@ -62,7 +74,8 @@ gulp.task(
                 )
             )
                 .filter(([_, stat]) => (stat as fs.Stats).isDirectory())
-                .map(([name, _]) => name as string);
+                .map(([name, _]) => name as string)
+                .filter((name) => EXCLUDED_DIRS.indexOf(name) === -1);
 
             // For every dir, bundle the index.js file
             return Promise.all(
@@ -73,16 +86,12 @@ gulp.task(
                                 mode: 'production',
                                 entry: path.join(
                                     __dirname,
-                                    './bundled',
+                                    dir,
                                     dir,
                                     'index.js'
                                 ),
                                 output: {
-                                    path: path.join(
-                                        __dirname,
-                                        './bundled',
-                                        dir
-                                    ),
+                                    path: path.join(__dirname, dir, dir),
                                     filename: 'index.js',
                                 },
                                 optimization: {
@@ -114,24 +123,87 @@ gulp.task(
                         '!**/*.png',
                     ],
                     {
-                        cwd: './bundled',
-                        base: './bundled',
+                        cwd: dir,
+                        base: dir,
                         read: false,
                     }
                 )
                 .pipe(gulpClean());
-        },
-        function changeHTML() {
-            // Change the overview index page to display
-            // different text depending on whether it's bundled or not
-            return gulp
-                .src(['index.html'], {
-                    cwd: './bundled',
-                    base: './bundled',
-                })
-                .pipe(replace(/hidden id="ifnotbundled"/g, 'id="ifnotbundled"'))
-                .pipe(replace(/id="ifbundled"/g, 'hidden id="ifbundled"'))
-                .pipe(gulp.dest('./'));
         }
+    );
+}
+
+// Bundle pages
+gulp.task(
+    'bundle',
+    gulp.series(bundleToDir('bundled'), function changeHTML() {
+        // Change the overview index page to display
+        // different text depending on whether it's bundled or not
+        return gulp
+            .src(['index.html'], {
+                cwd: './bundled',
+                base: './bundled',
+            })
+            .pipe(replace(/hidden id="ifnotbundled"/g, 'id="ifnotbundled"'))
+            .pipe(replace(/id="ifbundled"/g, 'hidden id="ifbundled"'))
+            .pipe(gulp.dest('./'));
+    })
+);
+
+// Bundle and server-side render pages
+gulp.task(
+    'ssr',
+    gulp.series(
+        bundleToDir('ssr'),
+        async function serverSideRender() {
+            // Get all directories in the bundled/ folder
+            const dirs = (
+                await Promise.all(
+                    (await fs.readdir(__dirname)).map(async (file) => {
+                        return [file, await fs.stat(file)];
+                    })
+                )
+            )
+                .filter(([_, stat]) => (stat as fs.Stats).isDirectory())
+                .map(([name, _]) => name as string)
+                .filter((name) => EXCLUDED_DIRS.indexOf(name) === -1);
+
+            return Promise.all(
+                dirs.map(async (dir) => {
+                    const imports = await import(
+                        path.join(__dirname, dir, `${dir}.js`)
+                    );
+                    const keys = Object.keys(imports).filter(
+                        (k) => k[0] === k[0].toUpperCase()
+                    );
+                    if (keys.length === 0) {
+                        throw new Error('No exports');
+                    }
+                    const mainExport = imports[keys[0]];
+
+                    // There's currently just one theme, use that one for all example elements
+                    const rendered = ssr(mainExport, {}, {}, theme['light']);
+
+                    const htmlPath = path.join(
+                        __dirname,
+                        'ssr',
+                        dir,
+                        'index.html'
+                    );
+                    const ssrHTML = await fs.readFile(htmlPath, {
+                        encoding: 'utf8',
+                    });
+
+                    const replaced = ssrHTML.replace(
+                        `<${mainExport.is}></${mainExport.is}>`,
+                        rendered
+                    );
+                    await fs.writeFile(htmlPath, replaced, {
+                        encoding: 'utf8',
+                    });
+                })
+            );
+        }
+        // TODO: changeHTML
     )
 );
